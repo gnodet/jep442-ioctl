829fea250c7aba472c55e2f8361ac805785fbfe5
Guillaume Nodet <gnodet@gmail.com>
2023-08-24 15:27:09 +0200

    Remove warning

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 javac --release 21 --enable-preview Reproducer.java
-java -cp . --enable-preview Reproducer
+java -cp . --enable-preview --enable-native-access=ALL-UNNAMED Reproducer
